4fa9e8ee9281f0b358c9e341beaf82a4ee9b5c56
tebogoyungmercykay <sbkskhalo.kq@gmail.com>
2025-02-03 22:47:47 +0200

    (clang-format) C++ Procedural Programming Sample Codes

--- skeleton_Code.cpp
+++ skeleton_Code.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 // using namespace std;
 int main() {
-    // Write C++ code here
-    std::cout << "Hello world!";
-    return 0;
+  // Write C++ code here
+  std::cout << "Hello world!";
+  return 0;
 }
