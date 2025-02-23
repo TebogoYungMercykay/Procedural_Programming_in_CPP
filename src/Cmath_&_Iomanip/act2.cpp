d7425f164b04cf109135d60261fbc1de932d0c48
tebogoyungmercykay <sbkskhalo.kq@gmail.com>
2025-02-23 22:53:01 +0200

    (clang-format) C++ Procedural Programming Sample Codes

--- src/Cmath_&_Iomanip/act2.cpp
+++ src/Cmath_&_Iomanip/act2.cpp
@@ -1,15 +1,14 @@
+#include <iomanip>
 #include <iostream>
 #include <string>
-#include <iomanip>
 
 using namespace std;
 
-int main()
-{
-string name;
-	cout << "Please enter your name: ";
-	getline( cin ,name);
-cout<<"Howdy: "<<setw(8)<<setfill('*')<<name<< endl;
-	
-	return 0;
+int main() {
+  string name;
+  cout << "Please enter your name: ";
+  getline(cin, name);
+  cout << "Howdy: " << setw(8) << setfill('*') << name << endl;
+
+  return 0;
 }
