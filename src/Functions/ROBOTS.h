a09fb168ebac96225b154b2f5410ebb48bd368a8
tebogoyungmercykay <sbkskhalo.kq@gmail.com>
2025-02-25 22:53:53 +0200

    (clang-format) C++ Procedural Programming Sample Codes

---
+++ src/Functions/ROBOTS.h
@@ -0,0 +1,15 @@
+#ifndef ROBOTS_H
+#define ROBOTS_H
+
+#include <sstream>
+#include <string>
+
+namespace ROBOTS {
+bool task1(std::string input);
+std::string task2(std::string input);
+std::string task3(std::string convertedInput);
+std::string task4(std::string translatedInput);
+std::string intToString(int v);
+} // namespace ROBOTS
+
+#endif /*ROBOTS_H*/
