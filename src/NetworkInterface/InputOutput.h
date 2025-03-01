e2040d6d4cc71a6960b4d4fd9a36d91a62431264
tebogoyungmercykay <sbkskhalo.kq@gmail.com>
2025-03-01 22:54:53 +0200

    (clang-format) Network Interface

---
+++ src/NetworkInterface/InputOutput.h
@@ -0,0 +1,11 @@
+#ifndef INPUT_OUTPUT_H
+#define INPUT_OUTPUT_H
+
+#include <iostream>
+#include <string>
+
+std::string askForStringInput(std::string message);
+int askForIntInput(std::string message);
+void printOut(std::string message);
+
+#endif /*INPUT_OUTPUT_H*/
