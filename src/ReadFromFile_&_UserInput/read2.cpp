0a59ec29f5164b45ef88f42a05ebacce0a083bff
tebogoyungmercykay <sbkskhalo.kq@gmail.com>
2025-03-22 23:00:41 +0200

    (clang-format) User Input and Reading from Files

--- src/ReadFromFile_&_UserInput/read2.cpp
+++ src/ReadFromFile_&_UserInput/read2.cpp
@@ -1,54 +1,47 @@
-#include <iostream>
-#include <fstream>
-#include <string>
 #include <cctype>
-#include <sstream>
+#include <fstream>
 #include <iomanip>
+#include <iostream>
+#include <sstream>
+#include <string>
 
 using namespace std;
 
-int main()
-{
-	int x=0;
-	int y;
-	int z;
-	string number;
-	string filename;
-	string halt="stop";
-	string frmFile;
-	string str1;
-	int w;
-	
-	cout <<"Enter the file name: ";
-	cin >>filename;
-	
-	ifstream linesFile;
-	linesFile.open(filename.c_str());
-	
-	while(!linesFile.eof())
-	{
-		if( linesFile >> number)
-		{
-			w=number.find(halt);
-			if( w != string::npos ){
-				cout<<"File reading stopped"<<endl;
-				break;
-			}
-			
-			for(int t=1;t<2;t++)
-			{
-				istringstream ss (number);
-				ss>>z;
-				y=z+x;
-				cout<<y<<endl;
-				x=z;
-			}
+int main() {
+  int x = 0;
+  int y;
+  int z;
+  string number;
+  string filename;
+  string halt = "stop";
+  string frmFile;
+  string str1;
+  int w;
 
-		}
-		
-	}
-	
-	linesFile.close();
-	return 0;
-	
+  cout << "Enter the file name: ";
+  cin >> filename;
+
+  ifstream linesFile;
+  linesFile.open(filename.c_str());
+
+  while (!linesFile.eof()) {
+    if (linesFile >> number) {
+      w = number.find(halt);
+      if (w != string::npos) {
+        cout << "File reading stopped" << endl;
+        break;
+      }
+
+      for (int t = 1; t < 2; t++) {
+        istringstream ss(number);
+        ss >> z;
+        y = z + x;
+        cout << y << endl;
+        x = z;
+      }
+    }
+  }
+
+  linesFile.close();
+  return 0;
 }
