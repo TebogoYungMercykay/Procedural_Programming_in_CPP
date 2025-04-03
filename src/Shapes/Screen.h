a70e8d3bc063cf987a677c3e9cc1ef1d44947f12
tebogoyungmercykay <sbkskhalo.kq@gmail.com>
2025-04-03 23:03:30 +0200

    (clang-format) Shapes and Screens

---
+++ src/Shapes/Screen.h
@@ -0,0 +1,18 @@
+#ifndef SCREEN_H
+#define SCREEN_H
+
+#include <iostream>
+
+namespace Screen {
+extern int nx;
+extern int ny;
+extern char **screenBuffer;
+
+void setUpScreen(int x, int y);
+void cleanScreen();
+void draw(int x, int y, char symbol = 'x');
+void printScreen();
+void cleanUpMemory();
+} // namespace Screen
+
+#endif /*SCREEN_H*/
