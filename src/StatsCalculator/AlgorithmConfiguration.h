ad5e463329ec9568da843dd2f6bcdffd0680bda1
tebogoyungmercykay <sbkskhalo.kq@gmail.com>
2025-04-13 23:05:15 +0200

    (clang-format) Stats Calculator

---
+++ src/StatsCalculator/AlgorithmConfiguration.h
@@ -0,0 +1,19 @@
+#ifndef ALGORITHM_CONFIGURATION_H
+#define ALGORITHM_CONFIGURATION_H
+
+#include <iostream>
+
+extern int START;
+extern int END;
+extern bool MOVETONEXT;
+
+extern float AVERAGE;
+extern float VALUE;
+
+void printStart();
+void printEnd();
+void printMoveToNext();
+void printAverage();
+void printValue();
+
+#endif // ALGORITHM_CONFIGURATION_H
