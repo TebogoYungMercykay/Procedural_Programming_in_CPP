e4f10f49ee22694380df3118a1d1605cbc8052a0
tebogoyungmercykay <sbkskhalo.kq@gmail.com>
2025-04-18 23:06:22 +0200

    (clang-format) TicTacToe Game

---
+++ src/TicTacToe/TicTacToe.h
@@ -0,0 +1,10 @@
+#ifndef TICTACTOE_H
+#define TICTACTOE_H
+
+#include "TicTacToeHelper.h"
+#include <iostream>
+
+int playGame();
+int askPlayerForPosition(std::string board, char symbol);
+
+#endif /*TICTACTOE_H*/
