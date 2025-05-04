84ca4c0655f1aed6947958ac586424e3be085639
tebogoyungmercykay <sbkskhalo.kq@gmail.com>
2025-05-05 00:01:52 +0200

    (update) Removed Unnecessary Comments

--- src/TicTacToe/TicTacToeHelper.h
+++ src/TicTacToe/TicTacToeHelper.h
@@ -5,29 +5,24 @@
 
 std::string generateEmptyBoard();
 
-// TASK 1 - 25%
 char getCell(std::string board, int cellIndex);
 std::string makeMove(std::string board, int position, char symbol);
 std::string prettyPrint(std::string board);
 
-// TASK 2 - 12%
 bool isValidMove(std::string board, int cell);
 bool isInValidMove(std::string board, int cell);
 bool isPlayerTurn(int turnCount, char symbol);
 bool isPlayerATurn(int turnCount);
 bool isPlayerBTurn(int turnCount);
 
-// TASK 3 - 10%
 bool rowCheck(std::string board, int row, char symbol);
 bool colCheck(std::string board, int col, char symbol);
 
-// TASK 4 - 32%
 bool isAWinner(std::string board);
 bool isBWinner(std::string board);
 bool boardFull(std::string board);
 bool gameOver(std::string board);
 
-// TASK 5 - 17%
 bool checkAllRows(std::string board, char symbol);
 bool checkAllCols(std::string board, char symbol);
 bool upwardDiagonalCheck(std::string board, char symbol);
