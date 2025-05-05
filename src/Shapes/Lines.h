8a673fb0bfce713e047df05c5c8e8dd5f7f4bfae
tebogoyungmercykay <sbkskhalo.kq@gmail.com>
2025-05-06 00:02:00 +0200

    (update) Removed Unnecessary Comments

--- src/Shapes/Lines.h
+++ src/Shapes/Lines.h
@@ -12,24 +12,13 @@ float absV(float v);
 
 float gradient(int p0x, int p0y, int p1x, int p1y);
 
-// Vertical line
 void drawLineA(int p0x, int p0y, int p1x, int p1y, char symbol = '*');
-
-// Horizontal line
 void drawLineB(int p0x, int p0y, int p1x, int p1y, char symbol = '*');
-
-// 0 < m < 1
 void drawLineC(int p0x, int p0y, int p1x, int p1y, char symbol = '*');
-
-//-1 < m < 0
 void drawLineD(int p0x, int p0y, int p1x, int p1y, char symbol = '*');
 
-// m >= 1
 void drawLineE(int p0x, int p0y, int p1x, int p1y, char symbol = '*');
-
-// m <= -1
 void drawLineF(int p0x, int p0y, int p1x, int p1y, char symbol = '*');
-
 void drawLine(int p0x, int p0y, int p1x, int p1y, char symbol = '*');
 
 #endif /*LINES_H*/
